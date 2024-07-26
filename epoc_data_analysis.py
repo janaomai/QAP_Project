a81812f2e5aec00d69b245ed7822973632955311
Jana <janaomaiche@gmail.com>
2024-07-26 13:43:43 +0930

    Add files via upload

--- epoc_data_analysis.py
+++ epoc_data_analysis.py
@@ -248,15 +248,15 @@ def run():
             your_result = site_data[analyte].values[0]
             if pd.isna(your_result):
                 row_cells[1].text = 'No submission'
-                row_cells[2].text = str(round(limits[analyte][0], 2))
-                row_cells[3].text = str(round(medians[analyte], 2))
-                row_cells[4].text = str(round(limits[analyte][1], 2))
+                row_cells[2].text = "{:2.f}".format(limits[analyte][0])
+                row_cells[3].text = "{:2.f}".format(medians[analyte])
+                row_cells[4].text = "{:2.f}".format(limits[analyte][1])
                 row_cells[5].text = y_labels[analyte]
             else:
-                row_cells[1].text = str(round(your_result, 2))
-                row_cells[2].text = str(round(limits[analyte][0], 2))
-                row_cells[3].text = str(round(medians[analyte], 2))
-                row_cells[4].text = str(round(limits[analyte][1], 2))
+                row_cells[1].text = "{:2.f}".format(your_result)
+                row_cells[2].text = "{:2.f}".format(limits[analyte][0])
+                row_cells[3].text = "{:2.f}".format(medians[analyte])
+                row_cells[4].text = "{:2.f}".format(limits[analyte][1])
                 row_cells[5].text = y_labels[analyte]
             if pd.isna(your_result):
                 row_cells[6].text = 'Unacceptable'
